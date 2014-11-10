5cd51b798897e22759939c44431ab19a76f03c3b
Dmitry Sadakov <sadakov@gmail.com>
2014-11-09 21:20:20 -0500

    pr is needed

--- js/popup.js
+++ js/popup.js
@@ -42,7 +42,7 @@ $('.subscribe-form').submit(function(e) {
   var $this = $(this);
   $.ajax({
       type: "POST", // GET & url for json slightly different
-      url: "http://APIStarter.us9.list-manage.com/subscribe/post-json?u=83c6f205a4379f5136e187d52&amp;id=cad0da3b8a&c=?",
+      url: "https://APIStarter.us9.list-manage.com/subscribe/post-json?u=83c6f205a4379f5136e187d52&amp;id=cad0da3b8a&c=?",
       data: $this.serialize(),
       dataType: 'jsonp',
       contentType: "application/json; charset=utf-8",

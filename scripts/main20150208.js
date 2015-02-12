87d8a64faf0b17508a1d618fe44d3e69f86fe54b
Dmitry Sadakov <sadakov@gmail.com>
2015-02-12 16:51:38 -0500

    Speech recognition

--- scripts/main20150208.js
+++ scripts/main20150208.js
@@ -3537,6 +3537,99 @@ function initSocialButtons() {
 	}));
 
 }
+// Dmitry Sadakov 2015 Voice module
+/*globals 
+          SpeechSynthesisUtterance: false
+*/
+
+/*exported voice */
+var voice = function ($, hue, colorUtil, sceneCmd) { 
+    
+    'use strict';
+	
+	var recognition = null;
+	var callback = null;
+
+	function speak(text){
+		if ('speechSynthesis' in window) {
+	      var speech = new SpeechSynthesisUtterance();
+	      speech.lang = 'en-US';
+	      speech.text = text;
+	      window.speechSynthesis.speak(speech);
+	  }
+	}
+
+	
+	function recognize(continuous, callbackFunc) { 
+		callback = callbackFunc;
+	    recognition.continuous = continuous;
+	    recognition.interimResults = false;
+
+		var SpeechRecognition = window.SpeechRecognition ||
+                            window.webkitSpeechRecognition ||
+                            window.mozSpeechRecognition ||
+                            window.msSpeechRecognition ||
+                            window.oSpeechRecognition;
+
+	    if (SpeechRecognition !== undefined) {
+	        recognition = new SpeechRecognition();
+	    }
+	    else {
+	        console.error('Your browser does not support the Web Speech API');
+	        return null;
+	    }
+
+        recognition.addEventListener('result', onSpeachResult);
+    }
+
+    function onSpeachResult(e) {
+    	var text = '';
+        for (var i = e.resultIndex; i < e.results.length; ++i) {
+            text += e.results[i][0].transcript;
+        }
+        if (callback) {
+        	callback(text);
+        }   
+    }
+
+    function start() {
+    	if (recognition) {
+        	recognition.start();
+    	}
+    }
+
+    function stop() {
+    	if (recognition) {
+        	recognition.stop();
+    	}
+    }
+
+    function abort() {
+    	if (recognition) {
+        	recognition.abort();
+    	}
+    }
+
+
+    return {
+        speak: function(text) {
+            return speak(text);
+        },
+        recognize: function() {
+            return recognize();
+        },
+        start: function() {
+            return start();
+        },
+        stop: function() {
+            return stop();
+        },
+        abort: function() {
+            return abort();
+        }
+    };
+};
+
 /**
  * Dmitry Sadakov's Philips Hue api wrapper, exposed as an AMD module.
  * Dependencies:
@@ -4849,7 +4942,7 @@ var hueCommander = function ($, hue, colorUtil, sceneCmd) {
 		      config:false,
           initSocialButtons: true,
           winapp: true,
-          SpeechSynthesisUtterance: false
+          voice: false
 */
 
 /* exported socialLikesButtons */
@@ -6204,12 +6297,8 @@ function initCloseMinimize() {
     });
 }
 
+
 $('#play-voice').click(function () {
   //<div id="voice-player" data-autoplay="true" data-text="Welcome to the jungle! hahaha just kidding!"></div>
-  if ('speechSynthesis' in window) {
-      var speech = new SpeechSynthesisUtterance();
-      speech.lang = 'en-US';
-      speech.text = 'You are awesome';
-      window.speechSynthesis.speak(speech);
-  }
+  voice.speak('You are awesome');
 });

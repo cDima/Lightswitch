8222cdc5943a58950397c79f67beaa662effb729
Dmitry Sadakov <sadakov@gmail.com>
2015-02-14 12:05:33 -0500

    New android build

---
+++ shine/platforms/android - Copy/ant-build/CordovaApp-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+c:\Projects\Lightswitch\shine\platforms\android\ant-build\CordovaApp-debug-unaligned.apk : \
+c:\Projects\Lightswitch\shine\platforms\android\ant-build\CordovaApp.ap_ \
+c:\Projects\Lightswitch\shine\platforms\android\ant-build\classes.dex \
+C:\Projects\Lightswitch\shine\platforms\android\CordovaLib\ant-build\classes.jar \
---
+++ shine/platforms/android - Copy/readme.txt
@@ -0,0 +1,7 @@
+to get android working:
+- copy 'android' to 'android - copy'
+- classpath 'com.android.tools.build:gradle:1.0.+'
+in CordovaLib and in root
+    compileSdkVersion 19
+    buildToolsVersion "19.1.0"
+distributionUrl=https\://services.gradle.org/distributions/gradle-1.10-all.zip

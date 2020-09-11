4e4342700aa2f2fc3637c22a9f96d0b1f51ae925
Dmitry Sadakov <sadakov@gmail.com>
2020-09-10 23:41:11 -0700

    Updating to 2020 pandemic
    gulp serve works

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,17 +9,12 @@
 // You can read more about the new JavaScript features here:
 // https://babeljs.io/docs/learn-es2015/
 
-import fs from 'fs';
-import path from 'path';
 import gulp from 'gulp';
 import del from 'del';
 import runSequence from 'run-sequence';
 import browserSync from 'browser-sync';
 import zip from 'gulp-zip';
-//import swPrecache from 'sw-precache';
 import gulpLoadPlugins from 'gulp-load-plugins';
-import {output as pagespeed} from 'psi';
-import pkg from './package.json';
 
 import rename from 'gulp-rename';
 import yargs from 'yargs';
@@ -27,6 +22,93 @@ import gulpif from 'gulp-if';
 
 const $ = gulpLoadPlugins();
 const reload = browserSync.reload;
+
+
+// Concatenate and minify JavaScript.
+
+var libs = [
+  './app/scripts/libs/babel-polyfill.min.js',
+  './app/scripts/libs/jquery-1.11.1.min.js',
+  './app/scripts/libs/bootstrap.min.js',
+  './app/scripts/libs/moment.min.js',
+  './app/scripts/libs/materialize.min.js',
+];
+var popupScr = [
+  './app/scripts/libs/bootstrap-slider.min.js',
+  './app/scripts/libs/color-thief.min.js',
+  './app/scripts/libs/extensions.js',
+  './app/scripts/ajaxlite.js',
+  './app/scripts/config.js',
+  './app/scripts/config.features.js',
+  './app/scripts/libs/ga.js',
+  './app/scripts/libs/storage.js',
+  './app/scripts/libs/uservoice.js',
+  './app/scripts/libs/colors.js',
+  './app/scripts/libs/palettes.js',
+  './app/scripts/libs/sequence.js',
+  './app/scripts/libs/ambient.js',
+  './app/scripts/libs/scenes.js',
+  './app/scripts/libs/sceneCommander.js',
+  //'./app/scripts/libs/testData.js',
+  './app/scripts/libs/hueTime.js',
+  './app/scripts/libs/hueDiscover.js',
+  './app/scripts/libs/hue.js',
+  './app/scripts/libs/colorUtil.js',
+  './app/scripts/libs/hueCommander.js',
+  './app/scripts/libs/voice.js', 
+  './app/scripts/libs/hueProxy.js',
+  './app/scripts/popup.js',
+
+];
+
+var backgroundScr = [
+  //'./app/scripts/libs/babel-polyfill.min.js',
+  './app/scripts/libs/jquery-1.11.1.min.js',
+  './app/scripts/libs/extensions.js',
+  './app/scripts/ajaxlite.js',
+  './app/scripts/config.js',
+  './app/scripts/config.features.js',
+  './app/scripts/libs/ga.js',
+  './app/scripts/libs/color-thief.min.js',
+  './app/scripts/libs/storage.js',
+  './app/scripts/libs/colors.js',
+  './app/scripts/libs/palettes.js',
+  './app/scripts/libs/sequence.js',
+  './app/scripts/libs/ambient.js',
+  './app/scripts/libs/scenes.js',
+  './app/scripts/libs/sceneCommander.js',
+  //'./app/scripts/libs/testData.js',
+  './app/scripts/libs/hueDiscover.js',
+  './app/scripts/libs/hue.js',
+
+  './app/scripts/libs/colorUtil.js',
+  './app/scripts/libs/hueCommander.js',
+
+  './app/scripts/libs/voice.js',
+  './app/scripts/libs/colorUtil.js',
+  './app/scripts/libs/hueCommander.js',
+  './app/scripts/background.js',
+];
+var tvosScr = [
+  './app/scripts/libs/browser-polyfill.min.js',
+  './app/scripts/libs/extensions.js',
+  './app/scripts/ajaxlite.js',
+  './app/scripts/libs/storage.js',
+  './app/scripts/libs/hueDiscover.js',
+  //'./app/scripts/libs/colors.js',
+  //'./app/scripts/libs/palettes.js',
+  //'./app/scripts/libs/sequence.js',
+  //'./app/scripts/libs/scenes.js',
+  //'./app/scripts/libs/sceneCommander.js',
+  './app/scripts/libs/hue.js',
+  './app/scripts/libs/colorUtil.js',
+  './app/scripts/libs/hueCommander.js',
+  './app/scripts/libs/hueProxy.js',
+  //'./app/scripts/popup.js'
+  './app/scripts/tvos/Presenter.js',
+  './app/scripts/tvos/ResourceLoader.js',
+  './app/scripts/tvos/application.js'
+];
 
 // Lint JavaScript
 gulp.task('lint', () =>
@@ -42,19 +124,11 @@ gulp.task('lint', () =>
 gulp.task('zip', function () {
     return gulp.src('dist/**')
         .pipe(zip('dist.zip'))
-        .pipe(gulp.dest(''));
+        .pipe(gulp.dest('./'));
 });
 // Optimize images
 gulp.task('images', () =>
   gulp.src('app/images/**/*')
-    .pipe(
-      //$.cache(
-        $.imagemin({
-          progressive: true,
-          interlaced: true
-        })
-        //)
-    )
     .pipe(gulp.dest('dist/images'))
     .pipe($.size({title: 'images'}))
 );
@@ -113,91 +187,6 @@ gulp.task('styles', () => {
     .pipe($.size({title: 'styles'}));
 });
 
-// Concatenate and minify JavaScript.
-
-var libs = [
-      './app/scripts/libs/babel-polyfill.min.js',
-      './app/scripts/libs/jquery-1.11.1.min.js',
-      './app/scripts/libs/bootstrap.min.js',
-      './app/scripts/libs/moment.min.js',
-      './app/scripts/libs/materialize.min.js',
-];
-var popupScr = [
-      './app/scripts/libs/bootstrap-slider.min.js',
-      './app/scripts/libs/color-thief.min.js',
-      './app/scripts/libs/extensions.js',
-      './app/scripts/ajaxlite.js',
-      './app/scripts/config.js',
-      './app/scripts/config.features.js',
-      './app/scripts/libs/ga.js',
-      './app/scripts/libs/storage.js',
-      './app/scripts/libs/uservoice.js',
-      './app/scripts/libs/colors.js',
-      './app/scripts/libs/palettes.js',
-      './app/scripts/libs/sequence.js',
-      './app/scripts/libs/ambient.js',
-      './app/scripts/libs/scenes.js',
-      './app/scripts/libs/sceneCommander.js',
-      //'./app/scripts/libs/testData.js',
-      './app/scripts/libs/hueTime.js',
-      './app/scripts/libs/hueDiscover.js',
-      './app/scripts/libs/hue.js',
-      './app/scripts/libs/colorUtil.js',
-      './app/scripts/libs/hueCommander.js',
-      './app/scripts/libs/voice.js', 
-      './app/scripts/libs/hueProxy.js',
-      './app/scripts/popup.js',
-
-    ];
-
-var backgroundScr = [
-      //'./app/scripts/libs/babel-polyfill.min.js',
-      './app/scripts/libs/jquery-1.11.1.min.js',
-      './app/scripts/libs/extensions.js',
-      './app/scripts/ajaxlite.js',
-      './app/scripts/config.js',
-      './app/scripts/config.features.js',
-      './app/scripts/libs/ga.js',
-      './app/scripts/libs/color-thief.min.js',
-      './app/scripts/libs/storage.js',
-      './app/scripts/libs/colors.js',
-      './app/scripts/libs/palettes.js',
-      './app/scripts/libs/sequence.js',
-      './app/scripts/libs/ambient.js',
-      './app/scripts/libs/scenes.js',
-      './app/scripts/libs/sceneCommander.js',
-      //'./app/scripts/libs/testData.js',
-      './app/scripts/libs/hueDiscover.js',
-      './app/scripts/libs/hue.js',
-
-      './app/scripts/libs/colorUtil.js',
-      './app/scripts/libs/hueCommander.js',
-
-      './app/scripts/libs/voice.js',
-      './app/scripts/libs/colorUtil.js',
-      './app/scripts/libs/hueCommander.js',
-      './app/scripts/background.js',
-    ];
-var tvosScr = [
-      './app/scripts/libs/browser-polyfill.min.js',
-      './app/scripts/libs/extensions.js',
-      './app/scripts/ajaxlite.js',
-      './app/scripts/libs/storage.js',
-      './app/scripts/libs/hueDiscover.js',
-      //'./app/scripts/libs/colors.js',
-      //'./app/scripts/libs/palettes.js',
-      //'./app/scripts/libs/sequence.js',
-      //'./app/scripts/libs/scenes.js',
-      //'./app/scripts/libs/sceneCommander.js',
-      './app/scripts/libs/hue.js',
-      './app/scripts/libs/colorUtil.js',
-      './app/scripts/libs/hueCommander.js',
-      './app/scripts/libs/hueProxy.js',
-      //'./app/scripts/popup.js'
-      './app/scripts/tvos/Presenter.js',
-      './app/scripts/tvos/ResourceLoader.js',
-      './app/scripts/tvos/application.js'
-    ];
 gulp.task('scriptsPopup', () => {
     return gulp.src(popupScr)
     //.pipe($.newer('.tmp/scripts'))
@@ -217,34 +206,36 @@ gulp.task('scriptsPopup', () => {
     .pipe($.size({title: 'scriptsPopup'}))
 });
 
-gulp.task('scripts', () => [
-      // Note: Since we are not using useref in the scripts build pipeline,
-      //       you need to explicitly list your scripts here in the right order
-      //       to be correctly concatenated
-      // Other scripts
-    gulp.src(libs)
+gulp.task('scripts-compile', () => {
+  return gulp.src(libs)
     .pipe($.concat('libs.min.20160220.js'))
     .pipe(gulp.dest('.tmp/scripts'))
     .pipe(gulp.dest('dist/scripts'))
     .pipe(gulp.dest('.tmp/scripts'))
-    .pipe($.size({title: 'scripts-libs'})),
+    .pipe($.size({title: 'scripts-libs'}));
 
+});
+gulp.task('scripts-background', () => {
+
+  return gulp.src(backgroundScr)
+  //.pipe($.newer('.tmp/scripts'))
+  .pipe($.sourcemaps.init())
+  .pipe($.babel())
+  .pipe($.sourcemaps.write())
+  .pipe(gulp.dest('.tmp/scripts'))
+  .pipe($.concat('background.min.js'))
+  // Output files
+  .pipe($.sourcemaps.write('.'))
+  .pipe(gulp.dest('dist/scripts'))
+  .pipe(gulp.dest('.tmp/scripts'))
+  .pipe($.size({title: 'scripts-bk'}));
+
+});
+gulp.task('scripts', gulp.series(
+    'scripts-compile',
     'scriptsPopup',
-
-    gulp.src(backgroundScr)
-    //.pipe($.newer('.tmp/scripts'))
-    .pipe($.sourcemaps.init())
-    .pipe($.babel())
-    .pipe($.sourcemaps.write())
-    .pipe(gulp.dest('.tmp/scripts'))
-    .pipe($.concat('background.min.js'))
-    // Output files
-    .pipe($.sourcemaps.write('.'))
-    .pipe(gulp.dest('dist/scripts'))
-    .pipe(gulp.dest('.tmp/scripts'))
-    .pipe($.size({title: 'scripts-bk'}))
-
-    ]
+    'scripts-background'
+  )
 );
 
 gulp.task('scripts-tvos', () => {
@@ -308,12 +299,8 @@ gulp.task('html', () => {
 gulp.task('clean', cb => del(['.tmp', 'dist/**/*', 'dist.zip', '!dist/.git'], {dot: true},
   cb));
 
-// Watch files for changes & reload
-gulp.task('serve', [
-  'scripts',
-  //'scripts-tvos',
-  'styles'], () => {
-  browserSync({
+gulp.task('browser-sync', () => {
+  browserSync.init({
     notify: false,
     // Customize the Browsersync console logging prefix
     logPrefix: 'WSK',
@@ -328,31 +315,18 @@ gulp.task('serve', [
   });
 
   gulp.watch(['app/**/*.html'], reload);
-  gulp.watch(['app/styles/**/*.{scss,css}'], ['styles', reload]);
-  gulp.watch(['app/scripts/**/*.js'], [
-    //'lint', 
-    'scriptsPopup',
-    //,'scripts-tvos'
-    reload
-    ]);
+  gulp.watch(['app/styles/**/*.{scss,css}'], gulp.series('styles', reload));
+  gulp.watch(['app/scripts/**/*.js'], gulp.series('scriptsPopup', reload));
   gulp.watch(['app/images/**/*'], reload);
-});
 
-// Build and serve the output from the dist build
-gulp.task('serve:dist', ['default'], () =>
-  browserSync({
-    notify: false,
-    logPrefix: 'WSK',
-    // Allow scroll syncing across breakpoints
-    scrollElementMapping: ['main', '.mdl-layout'],
-    // Run as an https by uncommenting 'https: true'
-    // Note: this uses an unsigned certificate which on first access
-    //       will present a certificate warning in the browser.
-    // https: true,
-    server: 'dist',
-    port: 3001
-  })
-);
+  });
+
+// Watch files for changes & reload
+gulp.task('serve', gulp.series(
+  gulp.parallel(
+    'scripts',
+    'styles'), 
+    'browser-sync'));
 
 function replaceConfigAndManifest(projectName) {
 	var config = 'app/scripts/config.' + projectName + '.js';
@@ -386,32 +360,33 @@ gulp.task('win', function() {
 });
 
 // Build production files, the default task
-gulp.task('default', ['clean'], cb =>
-  runSequence(
+gulp.task('default', 
+  gulp.series(
+    'clean',
     'styles',
-    [
+    gulp.parallel(
       //'lint', 
       'html', 
       'scripts', 
-      //'scripts-tvos',
-    'images', 
-    'fonts', 'copy'],
-    //'generate-service-worker',
-    'zip',
-    cb
-  )
+      'images', 
+      'fonts',
+      'copy'),
+  'zip')
 );
 
-// Run PageSpeed Insights
-gulp.task('pagespeed', cb =>
-  // Update the below URL to the public URL of your site
-  pagespeed('example.com', {
-    strategy: 'mobile'
-    // By default we use the PageSpeed Insights free (no API key) tier.
-    // Use a Google Developer API key if you have one: http://goo.gl/RkN0vE
-    // key: 'YOUR_API_KEY'
-  }, cb)
-);
 
-// Load custom tasks from the `tasks` directory
-// try { require('require-dir')('tasks'); } catch (err) { console.error(err); }
+// Build and serve the output from the dist build
+gulp.task('serve:dist', gulp.parallel('default'), () =>
+  browserSync({
+    notify: false,
+    logPrefix: 'WSK',
+    // Allow scroll syncing across breakpoints
+    scrollElementMapping: ['main', '.mdl-layout'],
+    // Run as an https by uncommenting 'https: true'
+    // Note: this uses an unsigned certificate which on first access
+    //       will present a certificate warning in the browser.
+    // https: true,
+    server: 'dist',
+    port: 3001
+  })
+);
